796659b464a5d2ab3e7f767074e73278f8e00e71
8Seph <evem-w@outlook.com>
2020-03-20 15:20:24 +0300

    Update ShopController.java

--- src/main/java/ru/mystore/store/controllers/ShopController.java
+++ src/main/java/ru/mystore/store/controllers/ShopController.java
@@ -23,14 +23,11 @@ public class ShopController {
 
     @RequestMapping(value = "/")
     public String orderByAvailable(Model model, @RequestParam Integer ava) {
-
-        System.out.println("ВОШЕЛ");
         //TODO сделать фильтр, который будет выводить фильтровать продукты по доступности. Выводить все продукты, но при этом указывать какие из них в наличие, а какие нет.
         if (ava == 0) {
             model.addAttribute("products", productService.findAllOrderByAvailable());
-            System.out.println("00000000000000000000000000000000000");
+
         } else {
-            System.out.println("11111111111111111111111111");
             model.addAttribute("products", productService.findAllOrderByAvailableDesc());
         }
 
