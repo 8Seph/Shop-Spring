3f2534801c0ffc28d1e8d0ec33385e3b84508344
8Seph <evem-w@outlook.com>
2020-03-26 15:38:51 +0300

    Prepared project

--- src/main/java/ru/mystore/store/exceptions/ImageExtensionException.java
+++
@@ -1,8 +0,0 @@
-package ru.mystore.store.exceptions;
-
-public class ImageExtensionException extends Exception {
-    private static final long serialVersionUID = 1L;
-    public ImageExtensionException(String msg) {
-        super(msg);
-    }
-}
