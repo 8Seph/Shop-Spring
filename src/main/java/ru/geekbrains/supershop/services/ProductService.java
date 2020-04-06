3e812ebb443781d8af65ffbefd1252aaa5e171df
8Seph <evem-w@outlook.com>
2020-04-06 16:44:07 +0300

    Update ProductService.java

--- src/main/java/ru/geekbrains/supershop/services/ProductService.java
+++ src/main/java/ru/geekbrains/supershop/services/ProductService.java
@@ -3,10 +3,12 @@ package ru.geekbrains.supershop.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.geekbrains.supershop.exceptions.EntityNotFoundException;
+import ru.geekbrains.supershop.persistence.entities.Country;
 import ru.geekbrains.supershop.persistence.entities.Image;
 import ru.geekbrains.supershop.persistence.entities.Product;
 import ru.geekbrains.supershop.persistence.entities.enums.ProductCategory;
@@ -14,6 +16,9 @@ import ru.geekbrains.supershop.persistence.pojo.ProductPojo;
 
 import ru.geekbrains.supershop.persistence.repositories.ProductRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +29,7 @@ import java.util.UUID;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final EntityManager entityManager;
 
     public Product findOneById(UUID uuid) throws EntityNotFoundException {
         return productRepository.findById(uuid).orElseThrow(
@@ -31,9 +37,49 @@ public class ProductService {
         );
     }
 
+//    public List<Product> findAll(Integer category) {
+//        return category == null ? productRepository.findAll() : productRepository.findAllByCategory(ProductCategory.values()[category]);
+//    }
+
     public List<Product> findAll(Integer category) {
-        return category == null ? productRepository.findAll() : productRepository.findAllByCategory(ProductCategory.values()[category]);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        //список того, чего хотим получить
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+
+        //корневая сущность к которой джойним или фильтруем
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        if (category == null) {
+            CriteriaQuery<Product> select = criteriaQuery.select(root);
+            return entityManager.createQuery(select).getResultList();
+        }
+
+        //параметры фильтра
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(criteriaBuilder.equal(root.get("category"), category));
+
+        //добавление параметров в запрос
+        criteriaQuery.where(criteriaBuilder.and(predicates.toArray(new Predicate[]{})));
+
+        return entityManager.createQuery(criteriaQuery).getResultList();
     }
+
+    public List<Product> findAllByCountry(Integer country) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+
+        Root<Product> root = criteriaQuery.from(Product.class);
+        Join<Product, Country> productCountryJoin = root.join("country");
+
+        List<Predicate> predicates = new ArrayList<>();
+        predicates.add(criteriaBuilder.equal(productCountryJoin.get("country"), country));
+
+        criteriaQuery.where(criteriaBuilder.and(predicates.toArray(new Predicate[]{})));
+        return entityManager.createQuery(criteriaQuery).getResultList();
+    }
+
 
     @Transactional
     public String save(ProductPojo productPogo, Image image) {
